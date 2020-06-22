ac1586fe4729e42abdfce02a267a6b634f296269
warbot <warbot.ted@gmail.com>
2020-06-21 19:33:01 -0700

    sync.sh: naming, source

--- sync.sh
+++ sync.sh
@@ -4,25 +4,25 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
-function doIt() {
+function syncDotfile() {
 	unison ~/dotfiles ~/\
 		 -ignore "Path {?*,.?*}"\
 		 -ignorenot "Name {.agignore,.aliases,.gitconfig,.vimrc,.zshrc}"\
      -batch
 
-  # source ~/.zshrc;
+  source ~/.zshrc;
 }
 
 # if [ "$1" == "--force" -o "$1" == "-f" ]; then
-# 	doIt;
+# 	syncDotfile;
 # else
 # 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 # 	echo "";
 # 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-# 		doIt;
+# 		syncDotfile;
 # 	fi;
 # fi;
 
-doIt;
-unset doIt;
+syncDotfile;
+unset syncDotfile;
 
