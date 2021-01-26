b65b334aa620a56acd407aa43030cea391c79712
warbot <warbot.ted@gmail.com>
2021-01-26 18:22:13 +0200

    tmux.conf

--- sync.sh
+++ sync.sh
@@ -13,7 +13,7 @@ function checkSyncTool() {
 function syncDotfile() {
 	unison ~/dotfiles ~/\
 		 -ignore "Path {?*,.?*}"\
-		 -ignorenot "Name {.agignore,.aliases,.gitconfig,.vimrc,.vim,.zshrc,.aliases}"\
+		 -ignorenot "Name {.agignore,.aliases,.gitconfig,.vimrc,.vim,.zshrc,.aliases,.tmux.conf}"\
      -batch
 
   source ~/.zshrc
